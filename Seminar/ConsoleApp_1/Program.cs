c6929ff85b32a32be7a636107bcf8dc1b360ee5e
Nikita Belov <lolvvto@yandex.ru>
2022-10-17 16:53:32 +0700

    Add ConsoleApp_1 and ConsoleApp_2

---
+++ Seminar/ConsoleApp_1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sqrNumber = number * number;
+
+Console.WriteLine("Квадрат вашего числа = " + sqrNumber);
